08f365f0f5c0caf8b905dd394bcbe5e5f1a96896
Liang <SulaFelinae@gmail.com>
2022-05-11 16:28:01 -0700

    adding to Xode for debug

---
+++ Zork/Zork/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Zork
+//
+//  Created by Liang Zhao on 5/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
